dcd782cb708ea2d5423aae2a55a763e366f2ebeb
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-22 15:24:44 -0300

    add mais configurações globais de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { Button } from './components/Button'
 import './styles/global.css'
 
 function App() {
   return (
     <div>
-      <Button color="red">
-        Botão 01
-      </Button>
-      <Button color="blue">
-        Botão 02
-      </Button>
-      <Button color="green">
-        Botão 03
-      </Button>
+
     </div>
   );
 }
