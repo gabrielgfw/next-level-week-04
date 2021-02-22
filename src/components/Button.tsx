c5ea266f1aa7f86e0fc13a51fed28892ef0ed893
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-22 14:31:17 -0300

    exemplo de componente com propriedade

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+// Props:
+
+// Estamos utilizando interfaces para definir as propriedades
+// do componente.
+
+// Para que o componente assuma a propriedade criada a partir
+// da interface, é necessário passar no parâmetro da função
+// que retorna o elemento em si, a propriedade via tipagem.
+
+interface ButtonProps {
+  color: string;
+}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button type="button">Botão <strong>Teste</strong></button>
+  );
+}
